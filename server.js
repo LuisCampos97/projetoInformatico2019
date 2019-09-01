983c7546d31b3be024bb6384c1b15c24826ca534
Jorge Correia <j_c@live.com.pt>
2019-09-01 15:58:34 +0100

    cors3

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 require('dotenv').config();
-var app = require('http').createServer();
+//var app = require('http').createServer();
 var io = require('socket.io')(app);
 
-app.use(function (req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', 'http://159.65.55.11');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-
-})
+ var app = require('http').createServer(function(req,res){
+// Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'http://159.65.55.11');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'UPGRADE, OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', req.header.origin);
+  if ( req.method === 'OPTIONS' || req.method === 'UPGRADE' ) {
+      res.writeHead(200);
+      res.end();
+      return;
+  }
+ });
 
 const nodemailer = require('nodemailer');
 
