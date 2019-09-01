8ebb63aa34c4f7d28ff5573af66f7fe17fc789f0
Jorge Correia <j_c@live.com.pt>
2019-09-01 03:18:27 +0100

    seeder para user admin

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'numero_identificacao' => NULL,
+            'tipo_documento' => NULL,
+            'name' => 'Administrador',
+            'username' => 1234567,
+            'roleDB' => 'admin',
+            'role' => 'Administrador',
+            'unidade_organica' => 'ESTG',
+            'password' => bcrypt('root'),
+            'blocked' => 0,
+            'deleted_at' => NULL,
+            'remember_token' => NULL,
+        ]);
+    }
+}
