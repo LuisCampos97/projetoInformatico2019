a2b8495e475de7de8ce0909c1ae351467f3b742e
Jorge Correia <j_c@live.com.pt>
2019-09-01 03:54:22 +0100

    cors error

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ require('dotenv').config();
 var app = require('http').createServer();
 var io = require('socket.io')(app);
 
+res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
